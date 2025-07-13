cb3dc9c61bb6f068d5e403688dad037ea30bbd94
Oliwier <145808327+m0lc14kk@users.noreply.github.com>
2025-07-13 14:46:12 +0200

    feat(scripts->clone): added more files and directories to clone script

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 [ -d ".github" ] && rm -rf .github
+[ -d "node_modules" ] && rm -rf node_modules
 [ -f ".hadolint.yaml" ] && rm .hadolint.yaml
+[ -f ".prettierrc.json" ] && rm .prettierrc.json
+[ -f ".prettierignore" ] && rm .prettierignore
+[ -f "package.json" ] && rm package.json
+[ -f "pnpm-lock.yaml" ] && rm pnpm-lock.yaml
 
 echo "Server's files has been cleaned up."
